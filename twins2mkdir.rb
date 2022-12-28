e5cb3502a2124cab6dff79bfa72217e2cb3f24f6
Yuzuki Arai <yudukikun5120@gmail.com>
2022-12-29 04:53:03 +0900

    build(version): 1.0.1

--- twins2mkdir.rb
+++ twins2mkdir.rb
@@ -1,8 +1,9 @@
 class Twins2mkdir < Formula
   desc "TWINS の履修科目を表す CSV ファイルから，科目ごとのディレクトリを作成するシェルスクリプト"
   homepage "https://github.com/yudukikun5120/twins2mkdir"
-  url "https://github.com/yudukikun5120/twins2mkdir/archive/refs/tags/v1.0.tar.gz"
-  sha256 "73d10dd13acd1a94f92a4b50f6bc5645225bfb9a2e930cfce1cd225d6d24b94d"
+  url "https://github.com/yudukikun5120/twins2mkdir/archive/refs/tags/v1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "3d1e90fe80fcd0f39d61d4386750bcdda39c522b81757e85ef89aa212937dfe1"
   license "MIT"
 
   depends_on "jq"
