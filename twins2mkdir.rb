b0fa20e602680a31fc8f9f83ec0cda526d635f4a
Yuzuki Arai <yudukikun5120@gmail.com>
2022-12-29 20:09:09 +0900

    fix: include submodules

--- twins2mkdir.rb
+++ twins2mkdir.rb
@@ -1,14 +1,20 @@
 class Twins2mkdir < Formula
   desc "TWINS の履修科目を表す CSV ファイルから，科目ごとのディレクトリを作成するシェルスクリプト"
   homepage "https://github.com/yudukikun5120/twins2mkdir"
-  url "https://github.com/yudukikun5120/twins2mkdir/archive/refs/tags/v1.0.1.tar.gz"
-  version "1.0.1"
+  url "https://github.com/yudukikun5120/twins2mkdir.git", branch: "main"
+  version "1.0.2"
   sha256 "3d1e90fe80fcd0f39d61d4386750bcdda39c522b81757e85ef89aa212937dfe1"
   license "MIT"
 
   depends_on "jq"
 
   def install
-    bin.install "twins2mkdir.sh" => "twins2mkdir"
+    inreplace "bin/twins2mkdir.sh",
+    "KDB_JSON=\"$(dirname \"$0\")/../lib/kdb-parse/kdb.json\"",
+    "KDB_JSON=\"#{lib}/kdb.json\""
+
+    bin.install "bin/twins2mkdir.sh" => "twins2mkdir"
+    lib.install "lib/color.sh" 
+    lib.install "lib/kdb-parse/kdb.json"
   end
 end
